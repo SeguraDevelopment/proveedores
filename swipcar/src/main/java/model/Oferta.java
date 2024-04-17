aa18482f83cd5aeaeadd3de9edce8177204a4e34
jpalmero <jpalmero@viewnext.com>
2024-04-17 17:32:06 +0200

    .,

---
+++ swipcar/src/main/java/model/Oferta.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Oferta {
+	private String vehiculo;
+	private String precio;
+	private String urlAnuncio;
+	private String tipo;
+	public String getVehiculo() {
+		return vehiculo;
+	}
+	public void setVehiculo(String vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	public String getUrlAnuncio() {
+		return urlAnuncio;
+	}
+	public void setUrlAnuncio(String urlAnuncio) {
+		this.urlAnuncio = urlAnuncio;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
