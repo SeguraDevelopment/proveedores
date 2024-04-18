3e8c718bba47be39b39ea168752bc07ee2805d8e
javier palmero <jpalm999@gmail.com>
2024-04-18 09:18:40 +0000

    Merged PR 1: get item

---
+++ swipcar/src/main/java/model/Oferta.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Oferta {
+	private String vehiculo;
+	private String precio;
+	private String urlAnuncio;
+	private String tipo;
+	private String urlFotoVehiculo;
+	public String getVehiculo() {
+		return vehiculo;
+	}
+	public void setVehiculo(String vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	public String getUrlAnuncio() {
+		return urlAnuncio;
+	}
+	public void setUrlAnuncio(String urlAnuncio) {
+		this.urlAnuncio = urlAnuncio;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getUrlFotoVehiculo() {
+		return urlFotoVehiculo;
+	}
+	public void setUrlFotoVehiculo(String urlFotoVehiculo) {
+		this.urlFotoVehiculo = urlFotoVehiculo;
+	}
+
+}
