b691a438cb98309b4251b38e5638c5c5be248ae5
jpalmero <jpalmero@viewnext.com>
2024-04-22 12:51:42 +0200

    .,

--- swipcar/src/main/java/model/ConfigApp.java
+++ swipcar/src/main/java/model/ConfigApp.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
@@ -7,19 +8,14 @@ import org.springframework.stereotype.Component;
 
 public class ConfigApp {
 	//private String urlProveedor;
-	private String proveedor;
+	
 	private String pathDriverFirefox;
 	private String pathFirefox;
 
-	private Map<String, String> flota;
-
-	public void setFlota(Map<String, String> flota) {
-	      this.flota = flota;
-	}
 	
-	public Map<String, String> getFlota() {
-	      return this.flota;
-	}
+	private List<Proveedor> proveedores;
+
+	
 	  
 	public String getPathDriverFirefox() {
 		return pathDriverFirefox;
@@ -29,23 +25,7 @@ public class ConfigApp {
 		this.pathDriverFirefox = pathDriverFirefox;
 	}
 
-	public String getProveedor() {
-		return proveedor;
-	}
-
-	public void setProveedor(String proveedor) {
-		this.proveedor = proveedor;
-	}
-
-	/*
-	public String getUrlProveedor() {
-		return urlProveedor;
-	}
-
-	public void setUrlProveedor(String urlProveedor) {
-		this.urlProveedor = urlProveedor;
-	}
-	*/
+	
 
 	public String getPathFirefox() {
 		return pathFirefox;
@@ -53,6 +33,14 @@ public class ConfigApp {
 
 	public void setPathFirefox(String pathFirefox) {
 		this.pathFirefox = pathFirefox;
+	}
+
+	public List<Proveedor> getProveedores() {
+		return proveedores;
+	}
+
+	public void setProveedores(List<Proveedor> proveedores) {
+		this.proveedores = proveedores;
 	}
 	
 
